d9e101664709855f220146f8304fa09d9b55a1a1
Vinh-Tuong <vinhtuong.mai@joingsg.com>
2024-03-30 12:50:42 +0100

    feat: search for words - continue

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <Head>
         <title>VocaX</title>
         <meta name="description" content="vocaX - German Wordlist - B1" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
